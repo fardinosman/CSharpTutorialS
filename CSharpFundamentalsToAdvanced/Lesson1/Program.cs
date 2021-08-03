3fa507a7e06dcb3157d4c9a600b766ce41db8644
Fardin Osman <osmanfardin@hotmail.dk>
2021-08-03 11:50:26 +0200

    lesson1

---
+++ CSharpFundamentalsToAdvanced/Lesson1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //intance of the class 
+            UsingData usingData = new UsingData();
+
+            Console.WriteLine(usingData.myAge = 22);
+            Console.WriteLine(usingData.myMoney = 22);
+            Console.WriteLine(usingData.myName = "Hello");
+
+            Console.WriteLine("The money is $ {0} and my name is {1} and my iam {2} years old", usingData.myMoney,
+                usingData.myName, usingData.myAge);
+            
+            Console.WriteLine(usingData.experi());
+
+            usingData.numbers();
+
+            Console.WriteLine("----------- encrement decrement counter exsample");
+            int counter = 5;
+            Console.WriteLine(counter );
+            Console.WriteLine(counter =  + 1);
+            Console.WriteLine(counter +=  1);
+            Console.WriteLine( counter = counter+1);
+            Console.WriteLine(counter++);
+            Console.WriteLine(++counter);
+            Console.WriteLine(counter--);
+            Console.WriteLine(--counter);
+            Console.WriteLine("-------------bool data type it can only hold 2 value and its value type its either true or false 1 or 0");
+            bool iItMonday = false;
+            bool areyouTired = true;
+            Console.WriteLine("--------------------------Comparsion operator with bool operator > < !> !> != ==");
+
+            int num1 = 5;
+            int num2 = 6;
+            bool mybool = num1 > num2;
+            mybool = num1 < num2;
+            mybool = num1 <= num2;
+            mybool = num1 == num2;
+            mybool = num1 != num2;
+            Console.WriteLine(mybool);
+
+            Console.WriteLine("-------------------------------Int- casting Double-----Decimal----Long--------------------------");
+            double myDouble = 5.5;
+            int myInt = (int)myDouble;
+            Console.WriteLine(myInt);
+
+            Console.WriteLine("-------------char---------exsample char hold single character----------");
+
+            char myChar = 'H';
+            Console.WriteLine(myChar);
+            char mySecondChar = '8';
+            Console.WriteLine(mySecondChar);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpFundamentalsToAdvanced/Lesson1/UsingData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    public class UsingData
+    {
+        public int myAge = 50; // int is value type
+        public double myMoney = 5.5; // double is value type
+        public string myName = "Fardin"; // string is reference type
+
+        int x = 5;
+        int y;
+        int modules = 10 % 3;//how many times it goes to 10 and rest is reminder
+
+        //operater precedence
+        //multication has higer precedence
+        //multi is first then division / - and +
+        int result = (2 + 3) * 4;
+
+        int counter = 5;
+        //counter= counter +1;
+        
+        public int experi()
+        {
+             y = 6 + x ;
+            return  y ;
+        }
+
+        public void numbers()
+        {
+            int x = 5;
+            int y = x * 5;
+            int z = x / 5;
+            Console.WriteLine("The value of x is  {0}" , x); 
+            Console.WriteLine("The value of y is  {0}",y ); 
+            Console.WriteLine("The value of z is  {0}",z);
+
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+        }
+    }
+}
