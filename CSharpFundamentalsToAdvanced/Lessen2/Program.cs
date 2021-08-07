4990d9094a55597e683314531842b6832b403607
Fardin Osman <osmanfardin@hotmail.dk>
2021-08-07 16:12:08 +0200

    lesson 3 loops

---
+++ CSharpFundamentalsToAdvanced/Lessen2/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessen2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 6;
+            //if (x == 5)
+            //    Console.WriteLine("X is 5");
+            //   Console.WriteLine("X is 5");
+            //   Console.WriteLine("X is 5");
+
+            //int number = 5;
+            //if (number ==5)
+            //{
+            //    Console.WriteLine("A");
+            //    Console.WriteLine("B");
+            //}
+
+            //bool myBool = true;
+            //if (myBool == false)
+            //{
+            //    Console.WriteLine("my boo is true");
+            //}
+            //const int LOW = 5;
+            //const int HIGH = 10;
+
+            //Console.WriteLine("Enter a number");
+
+            //int value = Convert.ToInt32(Console.ReadLine());
+
+            //if (value > LOW)
+            //{
+            //    if (value < HIGH)
+            //    {
+            //        Console.WriteLine("{0} is between  {1} and {2}",value,LOW,HIGH);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("enter number between {0} and {1}",LOW,HIGH);
+            //}
+            //Console.WriteLine("-----------nested if ---------------");
+            //int z = 3;
+            //bool Booly = false;
+            //if (z==6)
+            //{
+            //    if (Booly == true)
+            //    {
+            //        Console.WriteLine("Hellow");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("heloo");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Bye");
+            //}
+
+            //Console.WriteLine("Enter a number");
+
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //if (n > HIGH)
+            //{
+            //    Console.WriteLine("{0} is greater than {1}", n,HIGH);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("{0} is not greater than {1}", n, HIGH);
+            //}
+            //Console.WriteLine("Compartion Operator -----&&---´´----|| OR-------------------------");
+            //int a = 5;
+            //int b = 6;
+            //if (a ==5||b==7)
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("False");
+            //}
+
+            //Console.WriteLine("another && and ||  < > exsample");
+
+            //Console.WriteLine( "Enter your age");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter your name");
+            //string name = Console.ReadLine();
+            //if (age > 10 || name  =="tim")
+            //{
+            //    Console.WriteLine("Good time to marry");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not good");
+            //}
+            //Console.WriteLine("Enter a year");
+            //int year = Convert.ToInt32(Console.ReadLine());
+
+            //if (year ==1)
+            //{
+            //    Console.WriteLine("eid");
+            //}
+            //else if(year==2)
+            //{
+            //    Console.WriteLine("qurban");
+            //}
+            //else if (year == 3)
+            //{
+            //    Console.WriteLine("ramazan");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not match found");
+            //}
+            //Console.WriteLine("---------------------------Switch-------------------");
+
+            //switch (year)
+            //{
+            //    case 1:
+            //        {
+            //            Console.WriteLine("ramazan");
+            //            break;
+            //        }
+            //    case 2:
+            //        {
+            //            Console.WriteLine("shaban");
+            //            break;
+            //        }
+            //    case 3:
+            //        {
+            //            Console.WriteLine("eid");
+            //            break;
+            //        }
+            //    case 4: 
+            //        {
+            //            Console.WriteLine("qurban");
+            //            break;
+            //        }
+            //    default:
+            //        {
+            //            Console.WriteLine("no match found");
+            //          break;
+            //        }  
+
+            //}
+            Console.WriteLine("-------Program Task-----");
+
+            Console.WriteLine("lawn-mowing service");
+            Console.WriteLine("Please enter the lengdt");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the width");
+            double width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the number week");
+            double week = Convert.ToDouble(Console.ReadLine());
+
+            double mowing = length * width;
+
+            double weeklyPrice=0;
+          
+
+
+            if (mowing<=400 )
+            {
+                weeklyPrice = 25;
+              
+            }
+            else if (mowing > 400 && mowing <=600 )
+            {
+                weeklyPrice = 35;
+           
+            }
+            else if (mowing > 600 )
+            {
+                weeklyPrice = 50;
+               
+            }
+
+            double seasonPrice = weeklyPrice * 20;
+            Console.WriteLine(" weekly price is ${0} ", weeklyPrice);
+            Console.WriteLine("total fee for 20-week season is ${0}",seasonPrice);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpFundamentalsToAdvanced/Lesson 3 Looping/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3_Looping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a Amount");
+            double amount = double.Parse(Console.ReadLine());
+            double total = 0;
+
+            while (amount !=0)
+            {
+                total += amount;
+                Console.WriteLine("Enter next amount, or 0 to quit");
+                amount = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Your total is {0}", total.ToString("C"));
+            //-------------------------------------------------------------------------------------------
+            //double bankBal = 1000;
+            //const double INT_RATE = 0.04;
+            //char response;
+            //do
+            //{
+            //    Console.WriteLine("BankBalance is {0} ", bankBal.ToString("C"));
+            //    bankBal += bankBal * INT_RATE;
+            //    Console.WriteLine("do you want to see next year's balance? Y or N...");
+            //    response = char.Parse(Console.ReadLine());
+
+            //} while (response=='Y' || response =='y');
+            //Console.WriteLine("Have a Nice a day");
+
+            //-------------------------------------------------------------------------------------------
+            //int counter = 0;
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Hello {0} ", i);
+            //}
+
+            //-------------------------------------------------------------------------------------------
+
+            //int counter = 0;
+            //while (counter <10)
+            //{
+            //    Console.WriteLine("Hello {0} ",counter);
+            //    counter++;
+            //}
+            //-------------------------------------------------------------------------------------------
+            //double bankBal = 1000;
+            //const double INT_RATE = 0.04;
+
+            //Console.WriteLine("Do ypu want to see your balance? Y or N...");
+
+
+            //char response = char.Parse(Console.ReadLine());
+            //while (response == 'Y' || response == 'y')
+            //{
+            //    Console.WriteLine("Bank Balance is {0 }", bankBal.ToString("C"));
+            //    bankBal += bankBal * INT_RATE;
+            //    Console.WriteLine("Do you want to see the next year's balance Y or N...");
+
+            //    response = char.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("Have a Nice day");
+            //-------------------------------------------------------------------------------------------
+            //int number = 0;
+            //bool myBool = true;
+            //while (myBool)
+            //{
+            //    Console.WriteLine("Hello");
+            //    myBool = false;
+            //}
+        }
+    }
+}
